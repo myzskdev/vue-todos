3be2f6b7268b2b87004acdefa01019d807993349
myzsk <myzskdev@gmail.com>
2023-06-19 13:51:32 +0700

    first commit

---
+++ src/views/TodosView.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { ref } from 'vue'
+import { uid } from 'uid'
+import TodoCreator from '../components/TodoCreator.vue'
+import TodoItem from '../components/TodoItem.vue'
+import { Icon } from '@iconify/vue'
+
+const todoList = ref([])
+
+const createTodo = (todo) => {
+  todoList.value.push({
+    id: uid(),
+    todo,
+    isCompleted: null,
+    isEditing: null
+  })
+}
+</script>
+
+<template>
+  <main>
+    <h1>Create Todo</h1>
+    <TodoCreator @create-todo="createTodo" />
+    <ul class="todo-list" v-if="todoList.length > 0">
+      <TodoItem v-for="todo in todoList" :todo="todo" />
+    </ul>
+    <p class="todos-msg" v-else>
+      <Icon icon="noto-v1:sad-but-relieved-face" width="22" />
+      <span>You have no todo's to complete! Add one!</span>
+    </p>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  widows: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
+  }
+}
+</style>
